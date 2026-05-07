2f647903a86cf7599a1449bf9711c7688966a722
AbdulrahmanYasser27 <abdulrahmanyasser67@yahoo.com>
2026-05-08 02:26:45 +0300

    fix: fix shell escaping in pushImage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -5,7 +5,9 @@ def call(String imageName) {
         usernameVariable: 'DOCKER_USER',
         passwordVariable: 'DOCKER_PASS'
     )]) {
-        sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
-        sh "docker push ${imageName}"
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker push ${imageName}
+        """
     }
 }
