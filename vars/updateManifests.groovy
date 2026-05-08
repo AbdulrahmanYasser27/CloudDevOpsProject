2b434aa609f64d0463a835427a31829d220a242b
AbdulrahmanYasser27 <abdulrahmanyasser67@yahoo.com>
2026-05-09 02:58:36 +0300

    feat(jenkins): fresh Jenkinsfile and shared library

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,6 +1,4 @@
 def call(String imageName) {
     echo "Updating Kubernetes manifests with new image"
-    sh """
-        sed -i 's|image: abdulrahman1235/python-app:.*|image: ${imageName}|g' k8s/deployment.yaml
-    """
+    sh "sed -i 's|image: abdulrahman1235/python-app:.*|image: ${imageName}|g' k8s/deployment.yaml"
 }
