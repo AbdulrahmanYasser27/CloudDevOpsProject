affb373ebda5f090ab70a87dea565218110e9ac0
AbdulrahmanYasser27 <abdulrahmanyasser67@yahoo.com>
2026-05-09 02:46:47 +0300

    fix: fix variable scoping and branch name in pushManifests

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -5,12 +5,15 @@ def call() {
         usernameVariable: 'GIT_USER',
         passwordVariable: 'GIT_PASS'
     )]) {
+        def user = env.GIT_USER
+        def pass = env.GIT_PASS
+        def build = env.BUILD_NUMBER
         sh """
             git config user.email "jenkins@ci.com"
             git config user.name "Jenkins"
             git add k8s/deployment.yaml
-            git commit -m "ci: update image tag to ${BUILD_NUMBER}" || echo "Nothing to commit"
-            git push https://${GIT_USER}:${GIT_PASS}@github.com/AbdulrahmanYasser27/CloudDevOpsProject.git HEAD:master
+            git commit -m "ci: update image tag to ${build}" || echo "Nothing to commit"
+            git push https://${user}:${pass}@github.com/AbdulrahmanYasser27/CloudDevOpsProject.git HEAD:main
         """
     }
 }
